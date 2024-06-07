eacca8f998d5bc9d057145c857e9d7d1f1868213
Omer Akram <omer@thing.com>
2024-06-08 03:28:56 +0500

    go: fix register and unregister parsers

--- go/parsers/registered.go
+++ go/parsers/registered.go
@@ -7,16 +7,43 @@ import (
 	"github.com/xconnio/wampproto-protobuf/go/gen"
 )
 
-func RegisteredToProtobuf(registered *messages.Registered) ([]byte, error) {
-	return nil, nil
+type Registered struct {
+	gen *gen.Registered
 }
 
-func ProtobufToRegistered(data []byte) (*messages.Registered, error) {
-	msg := &gen.Registered{}
-	err := proto.Unmarshal(data[1:], msg)
+func NewRegisteredFields(gen *gen.Registered) messages.RegisteredFields {
+	return &Registered{gen: gen}
+}
+
+func (r *Registered) RequestID() int64 {
+	return r.gen.GetRequestId()
+}
+
+func (r *Registered) RegistrationID() int64 {
+	return r.gen.RegistrationId
+}
+
+func RegisteredToProtobuf(registered *messages.Registered) ([]byte, error) {
+	msg := &gen.Registered{
+		RequestId:      registered.RequestID(),
+		RegistrationId: registered.RegistrationID(),
+	}
+
+	data, err := proto.Marshal(msg)
 	if err != nil {
 		return nil, err
 	}
 
-	return nil, nil
+	byteValue := byte(messages.MessageTypeRegistered & 0xFF)
+	return append([]byte{byteValue}, data...), nil
+}
+
+func ProtobufToRegistered(data []byte) (*messages.Registered, error) {
+	msg := &gen.Registered{}
+	err := proto.Unmarshal(data, msg)
+	if err != nil {
+		return nil, err
+	}
+
+	return messages.NewRegisteredWithFields(NewRegisteredFields(msg)), nil
 }
