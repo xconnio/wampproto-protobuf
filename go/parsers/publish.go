b3e50cfd59faf86742dceabe6abb422190a67915
Muzzammil Shahid <muzzammil@xconn.io>
2024-06-10 16:39:31 +0500

    go: add publish parser

--- go/parsers/publish.go
+++ go/parsers/publish.go
@@ -1,14 +1,101 @@
 package parsers
 
 import (
+	"log"
+	"sync"
+
 	"google.golang.org/protobuf/proto"
 
 	"github.com/xconnio/wampproto-go/messages"
 	"github.com/xconnio/wampproto-protobuf/go/gen"
 )
 
+type Publish struct {
+	gen *gen.Publish
+
+	args   []any
+	kwArgs map[string]any
+	once   sync.Once
+}
+
+func NewPublishFields(gen *gen.Publish) messages.PublishFields {
+	return &Publish{gen: gen}
+}
+
+func (p *Publish) RequestID() int64 {
+	return p.gen.GetRequestId()
+}
+
+func (p *Publish) Options() map[string]any {
+	return nil
+}
+
+func (p *Publish) Topic() string {
+	return p.gen.GetTopic()
+}
+
+func (p *Publish) unpack() {
+	unpacked, err := FromCBORPayload(p.Payload())
+	if err != nil {
+		log.Println("error parsing CBOR payload:", err)
+	} else {
+		p.args = unpacked.Args()
+		p.kwArgs = unpacked.KwArgs()
+	}
+}
+
+func (p *Publish) Args() []any {
+	p.once.Do(p.unpack)
+	return p.args
+}
+
+func (p *Publish) KwArgs() map[string]any {
+	p.once.Do(p.unpack)
+	return p.kwArgs
+}
+
+func (p *Publish) PayloadIsBinary() bool {
+	return true
+}
+
+func (p *Publish) Payload() []byte {
+	return p.gen.GetPayload()
+}
+
+func (p *Publish) PayloadSerializer() int {
+	return int(p.gen.GetPayloadSerializer())
+}
+
 func PublishToProtobuf(publish *messages.Publish) ([]byte, error) {
-	return nil, nil
+	var msg *gen.Publish
+	if publish.PayloadIsBinary() {
+		msg = &gen.Publish{
+			RequestId:         publish.RequestID(),
+			Topic:             publish.Topic(),
+			PayloadSerializer: int32(publish.PayloadSerializer()),
+			Payload:           publish.Payload(),
+		}
+	} else {
+		payload, serializer, err := ToCBORPayload(publish)
+		if err != nil {
+			return nil, err
+		}
+
+		msg = &gen.Publish{
+			RequestId:         publish.RequestID(),
+			Topic:             publish.Topic(),
+			PayloadSerializer: int32(serializer),
+			Payload:           payload,
+		}
+	}
+
+	data, err := proto.Marshal(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	byteValue := byte(messages.MessageTypePublish & 0xFF)
+	return append([]byte{byteValue}, data...), nil
 }
 
 func ProtobufToPublish(data []byte) (*messages.Publish, error) {
@@ -18,5 +105,5 @@ func ProtobufToPublish(data []byte) (*messages.Publish, error) {
 		return nil, err
 	}
 
-	return nil, nil
+	return messages.NewPublishWithFields(NewPublishFields(msg)), nil
 }
