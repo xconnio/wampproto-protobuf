24ca65244740aaa46153708a9d838590a1edb896
Muzzammil Shahid <muzzammil@xconn.io>
2024-06-10 19:41:28 +0500

    go: add unregistered parser

--- go/parsers/unregistered.go
+++ go/parsers/unregistered.go
@@ -7,8 +7,30 @@ import (
 	"github.com/xconnio/wampproto-protobuf/go/gen"
 )
 
+type Unregistered struct {
+	gen *gen.UnRegistered
+}
+
+func NewUnregisteredFields(gen *gen.UnRegistered) messages.UnRegisteredFields {
+	return &Unregistered{gen: gen}
+}
+
+func (u *Unregistered) RequestID() int64 {
+	return u.gen.GetRequestId()
+}
+
 func UnregisteredToProtobuf(unregistered *messages.UnRegistered) ([]byte, error) {
-	return nil, nil
+	msg := &gen.UnRegistered{
+		RequestId: unregistered.RequestID(),
+	}
+
+	data, err := proto.Marshal(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	byteValue := byte(messages.MessageTypeUnRegistered & 0xFF)
+	return append([]byte{byteValue}, data...), nil
 }
 
 func ProtobufToUnregistered(data []byte) (*messages.UnRegistered, error) {
@@ -18,5 +40,5 @@ func ProtobufToUnregistered(data []byte) (*messages.UnRegistered, error) {
 		return nil, err
 	}
 
-	return nil, nil
+	return messages.NewUnRegisteredWithFields(NewUnregisteredFields(msg)), nil
 }
