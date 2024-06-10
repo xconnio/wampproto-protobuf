1ab3b42fa7968fbd71ebc610231f670f108cc097
Muzzammil Shahid <muzzammil@xconn.io>
2024-06-10 16:27:22 +0500

    go: add event parser

--- go/parsers/event.go
+++ go/parsers/event.go
@@ -7,8 +7,67 @@ import (
 	"github.com/xconnio/wampproto-protobuf/go/gen"
 )
 
+type Event struct {
+	gen *gen.Event
+	messages.BinaryPayload
+}
+
+func NewEventFields(gen *gen.Event) messages.EventFields {
+	return &Event{gen: gen}
+}
+
+func (e *Event) SubscriptionID() int64 {
+	return e.gen.SubscriptionId
+}
+
+func (e *Event) PublicationID() int64 {
+	return e.gen.PublicationId
+}
+
+func (e *Event) Details() map[string]any {
+	return nil
+}
+
+func (e *Event) Args() []any {
+	return nil
+}
+
+func (e *Event) KwArgs() map[string]any {
+	return nil
+}
+
+func (e *Event) PayloadIsBinary() bool {
+	return true
+}
+
+func (e *Event) Payload() []byte {
+	return e.gen.GetPayload()
+}
+
+func (e *Event) PayloadSerializer() int {
+	return int(e.gen.GetPayloadSerializer())
+}
+
 func EventToProtobuf(event *messages.Event) ([]byte, error) {
-	return nil, nil
+	payload, serializer, err := ToCBORPayload(event)
+	if err != nil {
+		return nil, err
+	}
+
+	msg := &gen.Event{
+		SubscriptionId:    event.SubscriptionID(),
+		PublicationId:     event.PublicationID(),
+		PayloadSerializer: int32(serializer),
+		Payload:           payload,
+	}
+
+	data, err := proto.Marshal(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	byteValue := byte(messages.MessageTypeEvent & 0xFF)
+	return append([]byte{byteValue}, data...), nil
 }
 
 func ProtobufToEvent(data []byte) (*messages.Event, error) {
@@ -18,5 +77,5 @@ func ProtobufToEvent(data []byte) (*messages.Event, error) {
 		return nil, err
 	}
 
-	return nil, nil
+	return messages.NewEventWithFields(NewEventFields(msg)), nil
 }
