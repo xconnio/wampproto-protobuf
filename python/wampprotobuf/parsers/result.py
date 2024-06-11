241445991db57e92d6d8d8cd14f549d686ad9b5f
Mahad <mahad@xconn.io>
2024-06-11 19:22:20 +0500

    add result parser

---
+++ python/wampprotobuf/parsers/result.py
@@ -0,0 +1,78 @@
+from typing import Any
+
+from wampproto.messages import Message
+from wampproto.messages.result import IResultFields, Result
+
+from wampprotobuf.gen import result_pb2
+from wampprotobuf.parsers import helpers
+
+
+class ResultFields(IResultFields):
+    def __init__(self, msg: result_pb2.Result):
+        super().__init__()
+        self._msg = msg
+        self._args = None
+        self._kwargs = None
+
+    @property
+    def request_id(self) -> int:
+        return self._msg.request_id
+
+    def unpack(self):
+        try:
+            args, kwargs = helpers.from_cbor_payload(self._msg.payload)
+            self._args = args
+            self._kwargs = kwargs
+        except Exception as e:
+            print(f"error parsing CBOR payload: {e}")
+
+    @property
+    def args(self) -> list[Any] | None:
+        return self._args
+
+    @property
+    def kwargs(self) -> dict[str, Any] | None:
+        return self._kwargs
+
+    @property
+    def options(self) -> dict[str, Any]:
+        return {}
+
+    def payload_is_binary(self) -> bool:
+        return True
+
+    @property
+    def payload(self) -> bytes | None:
+        return self._msg.payload
+
+    @property
+    def payload_serializer(self) -> int:
+        return self._msg.payload_serializer
+
+
+def from_protobuf(payload: bytes) -> Message:
+    result = result_pb2.Result()
+    result.ParseFromString(payload)
+
+    return Result(ResultFields(result))
+
+
+def to_protobuf(result: Result) -> bytes:
+    result_pb = result_pb2.Result()
+    result_pb.request_id = result.request_id
+
+    if result.payload_is_binary():
+        result_pb.payload = result.payload
+        result_pb.payload_serializer = result.payload_serializer
+    else:
+        try:
+            payload = helpers.to_cbor_payload(result.args, result.kwargs)
+        except Exception as e:
+            raise Exception(f"error in serialization to cbor: {e}")
+
+        serializer = 1
+
+        result_pb.payload = payload
+        result_pb.payload_serializer = serializer
+
+    return result_pb.SerializeToString()
