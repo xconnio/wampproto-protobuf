53a1d08087421b6df94b2cf42f32eec135586817
Muzzammil Shahid <muzzammil@xconn.io>
2024-07-06 19:24:21 +0500

    Adapt to latest changes in wampproto-python

--- python/wampprotobuf/parsers/unregistered.py
+++ python/wampprotobuf/parsers/unregistered.py
@@ -1,10 +1,10 @@
 from wampproto.messages import Message
-from wampproto.messages.unregistered import IUnRegisteredFields, UnRegistered
+from wampproto.messages.unregistered import IUnregisteredFields, Unregistered
 
 from wampprotobuf.gen import unregistered_pb2
 
 
-class UnRegisteredFields(IUnRegisteredFields):
+class UnRegisteredFields(IUnregisteredFields):
     def __init__(self, msg: unregistered_pb2.UnRegistered):
         super().__init__()
         self._msg = msg
@@ -18,10 +18,10 @@ def from_protobuf(payload: bytes) -> Message:
     result = unregistered_pb2.UnRegistered()
     result.ParseFromString(payload)
 
-    return UnRegistered(UnRegisteredFields(result))
+    return Unregistered(UnRegisteredFields(result))
 
 
-def to_protobuf(unregister: UnRegistered) -> bytes:
+def to_protobuf(unregister: Unregistered) -> bytes:
     result = unregistered_pb2.UnRegistered()
     result.request_id = unregister.request_id
 
--- python/wampprotobuf/parsers/unsubscribe.py
+++ python/wampprotobuf/parsers/unsubscribe.py
@@ -1,10 +1,10 @@
 from wampproto.messages import Message
-from wampproto.messages.unsubscribe import IUnSubscribeFields, UnSubscribe
+from wampproto.messages.unsubscribe import IUnsubscribeFields, Unsubscribe
 
 from wampprotobuf.gen import unsubscribe_pb2
 
 
-class UnSubscribeFields(IUnSubscribeFields):
+class UnSubscribeFields(IUnsubscribeFields):
     def __init__(self, msg: unsubscribe_pb2.UnSubscribe):
         self._msg = msg
 
@@ -21,10 +21,10 @@ def from_protobuf(payload: bytes) -> Message:
     result = unsubscribe_pb2.UnSubscribe()
     result.ParseFromString(payload)
 
-    return UnSubscribe(UnSubscribeFields(result))
+    return Unsubscribe(UnSubscribeFields(result))
 
 
-def to_protobuf(unsubscribe: UnSubscribe) -> bytes:
+def to_protobuf(unsubscribe: Unsubscribe) -> bytes:
     result = unsubscribe_pb2.UnSubscribe()
     result.request_id = unsubscribe.request_id
     result.subscription_id = unsubscribe.subscription_id
